48e2f5ecede4cf50dd3e50e179f5d3987d244d9d
happine2s <happine2s@sookmyung.ac.kr>
2022-07-21 02:26:17 +0900

    Fixed update.sh

--- deploy_v2/update.sh
+++ deploy_v2/update.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/RestAPI-Beginner'
+#PROJECT_BASE_PATH='/usr/local/apps/RestAPI-Beginner'
 
 git pull
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+/usr/local/virtualenvs/profiles_api/bin/python manage.py migrate
+/usr/local/virtualenvs/profiles_api/bin/python manage.py collectstatic --noinput
 supervisorctl restart profiles_api
 
 echo "DONE! :)"
